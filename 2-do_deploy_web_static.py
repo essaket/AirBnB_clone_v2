61f0c7517620b7785d18e78dde9f73b3c0d362fd
essaket <hichamessaket17gmail.com>
2024-05-03 12:28:55 -0700

    Writing task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,7 +3,7 @@
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['54.89.109.87', '100.25.190.21']
+env.hosts = ['100.26.220.120', '100.25.132.77']
 
 
 def do_deploy(archive_path):
