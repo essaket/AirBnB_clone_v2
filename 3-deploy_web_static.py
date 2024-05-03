70d55fca7bc1ff80e9b6b9a18e4014b93abeb403
essaket <hichamessaket17@gmail.com>
2024-05-03 16:07:48 -0700

    Writing task 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -9,7 +9,7 @@ execute: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/id_rsa -u ubuntu
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ["100.26.220.120", "100.25.132.77"]
+env.hosts = ['100.26.220.120', '100.25.132.77']
 
 def do_pack():
     """generates a tgz archive"""
